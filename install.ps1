53f374300eb39e96893ba2ce2d8b51387b325274
snqwq <99054951+snqwq@users.noreply.github.com>
2024-07-08 04:29:05 -0500

    first commit stuff

---
+++ install.ps1
@@ -0,0 +1,32 @@
+# System Configuration
+Set-ExplorerOptions -showHiddenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
+
+
+# Chocolatey Installs
+choco install winfetch
+choco install microsoft-windows-terminal
+
+choco install 7zip.install
+
+choco install firefox
+choco install googlechrome
+
+choco install notepadplusplus.install
+choco install sublime-text
+
+choco install python
+choco install nodejs
+
+choco install vlc.install
+
+choco install transmission
+
+# Debloat
+& ([scriptblock]::Create((irm "https://raw.githubusercontent.com/Raphire/Win11Debloat/master/Get.ps1"))) -RunDefaults -Silent -RemoveW11Outlook
+
+# Change wallpaper
+$wallpaper = "C:\Users\$env:UserName\Pictures\wallpaper.jpg"
+$wallpaperUrl = "https://i.imgur.com/1Z9Q6ZC.jpg"
+Invoke-WebRequest -Uri $wallpaperUrl -OutFile $wallpaper
+
+set-itemproperty -path "HKCU:Control Panel\Desktop" -name WallPaper -value $wallpaper
